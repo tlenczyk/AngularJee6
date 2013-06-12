5e72a53795a8e3d6afa85d506947901992343c85
tomek <tomek@8470p>
2013-06-12 23:22:26 +0200

    Initial version of  price filter

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -3,8 +3,16 @@
 /* Filters */
 
 angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+        filter('priceFromFilter', function() {
+    return function(items, priceFrom, priceTo) {
+        var priceFrom = parseInt(priceFrom);
+        var priceTo = parseInt(priceTo);
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            if (item.price >= priceFrom && item.price <= priceTo) {
+                filtered.push(item);
+            }
+        });
+        return filtered;
+    };
+});
